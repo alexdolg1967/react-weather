7ee5f7815d0f14b005e25f2e500abbe637c0da89
alexdolg1967 <alexdolg1967@mail.ru>
2021-09-23 20:12:54 +0300

    add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import { Home } from './pages/Home/Home';
+import { Mountch } from './pages/Mountch/Mountch';
 
 function App() {
     return (
         <div className="App">
-            <h1>Ghbdtn</h1>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/mountch" exact component={Mountch} />
+            </Switch>
         </div>
     );
 }
