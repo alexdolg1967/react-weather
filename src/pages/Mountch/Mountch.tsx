758c9efb35b9fe28cad91e48e3f12c3d353aeb44
alexdolg1967 <alexdolg1967@mail.ru>
2021-09-23 20:04:32 +0300

    add pages - Home & Mountch

---
+++ src/pages/Mountch/Mountch.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import s from './Mountch.module.scss'
+
+interface Props {
+
+}
+
+export const Mountch = (props: Props) => {
+    return (
+        <div className={s.mountch}>
+            <h2>Mountch</h2>
+        </div>
+    )
+}
