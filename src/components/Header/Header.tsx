a009a5585f23d1fcd52d781c5e6866a4577777cf
alexdolg1967 <alexdolg1967@mail.ru>
2021-09-23 20:48:59 +0300

    add logo & change-theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { GlobalSvgSelector } from '../../assets/icons/global/GlobalSvgSelector'
 import s from './Header.module.scss'
 
 interface Props {
@@ -8,7 +9,17 @@ interface Props {
 export const Header = (props: Props) => {
     return (
         <div className={s.header}>
-            Header
+            <div className={s.wrapper}>
+                <div className={s.logo}>
+                    <GlobalSvgSelector id="header-logo" />
+                </div>
+                <div className={s.title}>React weather</div>
+            </div>
+            <div className={s.wrapper}>
+                <div className={s.change_theme}>
+                    <GlobalSvgSelector id="change-theme" />
+                </div>
+            </div>
         </div>
     )
 }
