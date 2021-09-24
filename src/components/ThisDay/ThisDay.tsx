09c8eb33140f2b98f604027274da276aef36a9e8
alexdolg1967 <alexdolg1967@mail.ru>
2021-09-24 10:42:33 +0300

    ends ThisDay

--- src/components/ThisDay/ThisDay.tsx
+++ src/components/ThisDay/ThisDay.tsx
@@ -10,8 +10,11 @@ export const ThisDay = (props: Props) => {
     return (
         <div className={s.this__day}>
             <div className={s.top__block}>
-                <div className={s.temp}>20°</div>
-                <div className={s.day}>Сегодня</div>
+                <div className={s.wrapper}>
+					<div className={s.temp}>20°</div>
+                	<div className={s.day}>Сегодня</div>
+				</div>
+				<GlobalSvgSelector id="sun"/>
             </div>
             <div className={s.bottom__block}>
                 <div className={s.time}>Время: <span>21:54</span></div>
