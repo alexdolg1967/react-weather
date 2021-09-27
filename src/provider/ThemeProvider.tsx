cf7988d11e864c4d29beec31a41a19b5174f1c4b
alexdolg1967 <alexdolg1967@mail.ru>
2021-09-27 11:56:03 +0300

    edit css home

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode, useState } from 'react'
 import { Theme, ThemeContext } from '../Context/ThemeContext'
 import { changeCssRootVariables } from '../model/ChangeCssRootVariables'
+import { storage } from '../model/Storage'
 
 interface Props {
 	children: ReactNode;
@@ -8,7 +9,7 @@ interface Props {
 
 export const ThemeProvider = ({children, ...props}: Props) => {
 
-	const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
+	const [theme, setTheme] = useState<Theme>(storage.getItem('theme') || Theme.LIGHT)
 
 	function changeTheme(theme: Theme) {
 		setTheme(theme)
