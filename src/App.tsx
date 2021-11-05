b556aa19f73dc1531046cf8ee202ed282667ce5f
alexdolg1967 <alexdolg1967@mail.ru>
2021-11-05 16:49:13 +0300

    add BrowoserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
+import { BrowserRouter, Route, Switch } from 'react-router';
 import { Header } from './components/Header/Header';
 import { Popup } from './components/Popup/Popup';
 import { Home } from './pages/Home/Home';
@@ -7,14 +7,16 @@ import { Mountch } from './pages/Mountch/Mountch';
 
 function App() {
     return (
-        <div className="container">
-            {/* <Popup /> */}
-            <Header />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/mountch" exact component={Mountch} />
-            </Switch>
-        </div>
+        <BrowserRouter basename="/">
+            <div className="container">
+                {/* <Popup /> */}
+                <Header />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/mountch" exact component={Mountch} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
